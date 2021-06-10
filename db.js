cfab1ba8bca24663bb73d796956e8b9953f6bc81
Sean Oyler <xrysen@gmail.com>
2021-06-10 13:47:49 -0700

    Initial commit

---
+++ db.js
@@ -0,0 +1,188 @@
+require("dotenv").config();
+
+const { Pool } = require("pg");
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+const pool = new Pool({
+  connectionString: connectionString,
+});
+
+const getUserByEmail = (email) => {
+  return pool.query(`SELECT users.id FROM users WHERE email = '${email}'`);
+}
+
+const addNewUser = (email, name) => {
+  pool.query(`INSERT INTO users (email, first_name) VALUES ($1, $2)`, [email, name]);
+}
+
+const getAllTypes = (request, response) => {
+  pool.query("SELECT * FROM food_type", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.send(results.rows);
+  });
+};
+
+const getAllIngredients = (request, response) => {
+  pool.query("SELECT * FROM ingredient ORDER BY type_id;", (error, results) => {
+    response.send(results.rows);
+  });
+};
+
+const getLastRecipeId = () => {
+  return pool.query("SELECT recipe.id FROM recipe ORDER BY id DESC LIMIT 1");
+};
+
+const getRecipeById = (id) => {
+  return pool.query(`SELECT recipe.id, recipe.name, ingredient_list.ingredient_id, ingredient_list.measurement, ingredient.name AS ingredient FROM recipe 
+  JOIN ingredient_list ON recipe.id = recipe_id
+  JOIN ingredient ON ingredient_list.ingredient_id = ingredient.id
+  WHERE recipe.id = ${id}`);
+};
+
+const getRecipeDirectionsById = (id) => {
+  return pool.query(`SELECT directions FROM direction WHERE recipe_id = ${id}`);
+};
+
+const getAllDirections = () => {
+  return pool.query("SELECT * FROM direction;");
+}
+
+const getAllRecipes = () => {
+  return pool.query(`SELECT * FROM recipe;`);
+}
+
+const getRecipesByCat = (catId) => {
+  return pool.query(`SELECT * FROM recipe WHERE category_id = $1`, [catId]);
+}
+
+const getRecipesByUserId = (userId) => {
+  return pool.query(`SELECT * FROM recipe WHERE user_id = ${userId}`);
+}
+
+const addNewIngredient = (name, type, res) => {
+  pool.query(
+    `INSERT INTO ingredient (name, type_id) 
+    VALUES ($1, $2);
+    `,
+    [name, type]
+  );
+  res.send("Success!");
+};
+
+const addNewRecipe = (name, leftovers, userId) => {
+ return pool.query(
+    `INSERT INTO recipe (name, has_leftovers, user_id)
+    VALUES ($1, $2, $3)`,
+    [name, leftovers, userId]
+  );
+};
+
+const updateIngredientList = (recipeID, ingredientID, measurement) => {
+  pool.query(
+    `
+    INSERT INTO ingredient_list (recipe_id, ingredient_id, measurement)
+    VALUES ($1, $2, $3)
+    `,
+    [recipeID, ingredientID, measurement]
+  );
+};
+
+const getIngredientsByType = (typeId) => {
+  return pool.query(
+    `SELECT * FROM ingredient WHERE type_id = $1`, [typeId]
+  )
+}
+
+const updateDirections = (recipeID, direction) => {
+  pool.query(
+    `
+    INSERT INTO direction (recipe_id, directions)
+    VALUES ($1, $2)
+    `,
+    [recipeID, direction]
+  );
+};
+
+const getCategories = () => {
+ return pool.query(
+    `SELECT * FROM category;`
+  )
+}
+
+const getFoodTypes = () => {
+  return pool.query("SELECT * FROM food_type;");
+}
+
+const resetRecipes = () => {
+  pool.query(
+    `DROP TABLE IF EXISTS recipe CASCADE;
+    CREATE TABLE recipe (
+      id SERIAL PRIMARY KEY NOT NULL,
+      name VARCHAR(255) NOT NULL
+    );
+    `
+  );
+};
+
+const resetDirections = () => {
+  pool.query(
+    `DROP TABLE IF EXISTS direction CASCADE;
+    CREATE TABLE direction (
+      id SERIAL PRIMARY KEY NOT NULL,
+      directions TEXT NOT NULL,
+      recipe_id INTEGER REFERENCES recipe(id) ON DELETE CASCADE
+    );`
+  );
+};
+
+const resetIngredientList = () => {
+  pool.query(
+    `
+    DROP TABLE IF EXISTS ingredient_list CASCADE;
+    CREATE TABLE ingredient_list (
+      id SERIAL PRIMARY KEY NOT NULL,
+      recipe_id INTEGER REFERENCES recipe(id) ON DELETE CASCADE,
+      ingredient_id INTEGER REFERENCES ingredient(id) ON DELETE CASCADE,
+      measurement VARCHAR(255)
+    );
+    `
+  );
+};
+
+const deleteRecipe = (recipeId) => {
+  pool.query(
+    `DELETE FROM recipe WHERE id = $1`, [recipeId]
+  );
+}
+
+const resetDB = () => {
+  resetRecipes();
+  resetDirections();
+  resetIngredientList();
+};
+
+module.exports = {
+  getAllTypes,
+  addNewIngredient,
+  getAllIngredients,
+  addNewRecipe,
+  resetDB,
+  getLastRecipeId,
+  updateIngredientList,
+  getRecipeById,
+  updateDirections,
+  getRecipeDirectionsById,
+  getAllRecipes,
+  getCategories,
+  getRecipesByCat,
+  getFoodTypes,
+  getIngredientsByType,
+  getAllDirections,
+  getUserByEmail,
+  addNewUser,
+  getRecipesByUserId,
+  deleteRecipe
+};
---
+++ server.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const db = require("./db");
+const PORT = 8080 || process.env.PORT;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  db.addNewIngredient("Tomato", 3, res);
+});
+
+app.get("/ingredients", db.getAllIngredients);
+app.post("/ingredients", (req, res) => {
+  db.addNewIngredient(req.body.name, req.body.category, res);
+});
+
+app.get("/ingredients/:typeId", (req, res) => {
+  db.getIngredientsByType(req.params.typeId).then((result) =>
+    res.send(result.rows)
+  );
+});
+
+app.get("/reset", (req, res) => {
+  db.resetDB();
+  res.send("Database reset");
+});
+
+app.get("/recipes/:id", (req, res) => {
+  db.getRecipeById(req.params.id)
+    .then((result) => res.send(result.rows))
+    .catch((err) => console.log(err));
+});
+
+app.get("/directions/:id", (req, res) => {
+  db.getRecipeDirectionsById(req.params.id)
+    .then((result) => res.send(result.rows))
+    .catch((err) => console.log(err));
+});
+
+app.get("/directions", (req, res) => {
+  db.getAllDirections().then((result) => res.send(result.rows));
+});
+
+app.get("/recipes", (req, res) => {
+  db.getAllRecipes().then((result) => res.send(result.rows));
+});
+
+app.get("/recipes/users/:userId", (req, res) => {
+  db.getRecipesByUserId(req.params.userId).then((result) => res.send(result.rows));
+})
+
+app.get("/categories", (req, res) => {
+  db.getCategories().then((result) => res.status(200).send(result.rows));
+});
+
+app.get("/categories/:id", (req, res) => {
+  db.getRecipesByCat(req.params.id).then((result) =>
+    res.status(200).send(result.rows)
+  );
+});
+
+app.get("/users/", (req, res) => {
+  db.getUserByEmail(req.query.email).then((result) => {
+    if(result.rows.length) {
+      res.status(200).send(result.rows);
+    } else {
+      res.json([{error: "User doesn't exist"}]);
+    }
+  }).catch(err => console.log(err));
+})
+
+app.post("/users/", (req, res) => {
+  db.addNewUser(req.body.email, req.body.name);
+  res.status(200).send("Succesfully Added new user");
+})
+
+app.post("/recipes", (req, res) => {
+  db.addNewRecipe(req.body.name, req.body.leftovers, req.body.userId).then((res) => {
+    db.getLastRecipeId().then((res) => {
+      if (res.rows[0].id) {
+        for (const item of req.body.ingredients) {
+          db.updateIngredientList(
+            res.rows[0].id,
+            item.ingredient,
+            item.measurement
+          );
+          for (const item of req.body.directions) {
+            db.updateDirections(res.rows[0].id, item);
+          }
+        }
+      }
+    });
+  });
+});
+
+app.delete("/recipes/:id", (req, res) => {
+  db.deleteRecipe(req.params.id);
+  res.status(200).send("Successfully deleted recipe");
+})
+
+app.get("/food_types", (req, res) => {
+  db.getFoodTypes().then((result) => res.status(200).send(result.rows));
+});
+
+app.listen(PORT, () => {
+  console.log("Server started listening on port " + PORT);
+});
