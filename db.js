dbec441eca92ac8983c7684981c778ff5239fcfb
Sean Oyler <xrysen@gmail.com>
2021-06-10 14:22:32 -0700

    Gets server ready for deployment

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString = HEROKU_POSTGRESQL_OLIVE_URL;
 
 const pool = new Pool({
   connectionString: connectionString,
